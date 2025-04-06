541115682bec478d1a5e1bf5e9ec427fe60b8f68
Palina Mialyantsava <linamel08v@gmail.com>
2025-04-06 15:28:26 +0000

    changes

--- salon.sh
+++ salon.sh
@@ -18,6 +18,7 @@ MAIN_MENU() {
     echo "$SERVICE_ID_TRIMMED) $NAME_TRIMMED"
   done
   read SERVICE_ID_SELECTED
+  SERVICE_ID_SELECTED=$(echo $SERVICE_ID_SELECTED | sed 's/ //g')
   SERVICE_NAME_SELECTED=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED");
   SERVICE_NAME_SELECTED=$(echo $SERVICE_NAME_SELECTED | sed -E 's/^ *| *$//g')
   if [[ -z $SERVICE_NAME_SELECTED ]]
@@ -40,16 +41,17 @@ CHOOSE_MENU() {
     echo -e "\nI don't have a record for that phone number, what's your name?"
     read CUSTOMER_NAME
     #insert new customer
-    FORMATTED_CUSTOMER_NAME=$(echo $CUSTOMER_NAME | sed -E  's/^ *| *$//g')
-    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone,name) VALUES ('$CUSTOMER_PHONE','$FORMATTED_CUSTOMER_NAME') ")
+    CUSTOMER_NAME=$(echo $CUSTOMER_NAME | sed -E 's/^ *| *$//g')
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone,name) VALUES ('$PHONE_NUMBER','$CUSTOMER_NAME') ")
   fi
   # get customer_id
   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE' ")
+  CUSTOMER_ID=$(echo $CUSTOMER_ID | sed 's/ //g')
   #get time
-  echo -e "\nWhat time would you like your $SERVICE_NAME_SELECTED, $FORMATTED_CUSTOMER_NAME?"
+  echo -e "\nWhat time would you like your $SERVICE_NAME_SELECTED, $CUSTOMER_NAME?"
   read SERVICE_TIME
   INSERT_TIME_RESULT=$($PSQL "INSERT INTO appointments(customer_id,service_id, time) VALUES ('$CUSTOMER_ID','$SERVICE_ID_SELECTED','$SERVICE_TIME') ")
-  echo -e "\nI have put you down for a $SERVICE_NAME_SELECTED at $SERVICE_TIME, $FORMATTED_CUSTOMER_NAME." 
+  echo -e "\nI have put you down for a $SERVICE_NAME_SELECTED at $SERVICE_TIME, $CUSTOMER_NAME." 
 
 }
 MAIN_MENU
